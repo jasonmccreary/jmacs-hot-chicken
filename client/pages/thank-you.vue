253dbe1ddf2fa6e48276a7ea071c609d0ff9022c
Jason McCreary <jason@pureconcepts.net>
2018-08-17 11:36:46 -0500

    Add thank you page

---
+++ client/pages/thank-you.vue
@@ -0,0 +1,18 @@
+<template>
+  <main-layout>
+    <div>
+      <h2>Thank you!</h2>
+      <p>We're pluckin' that mothercluckin' bird right now…<br><br><strong>Bring your credit card</strong> to pick up your order. Kind of like Redbox. But you won't get a DVD.</p>
+    </div>
+  </main-layout>
+</template>
+
+<script>
+  import MainLayout from '../layouts/Main.vue'
+
+  export default {
+    components: {
+      MainLayout
+    }
+  }
+</script>
--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,4 @@
 export default {
-  '/': 'home'
+  '/': 'home',
+  '/thank-you': 'thank-you'
 }
